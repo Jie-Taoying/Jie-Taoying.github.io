f0499550fb2c602acbf4f55295c5b703b8ee3f64
Jie-Taoying <2026125544@qq.com>
2025-07-28 15:09:27 +0800

    1

--- docs/js/analytics.js
+++
@@ -1,9 +0,0 @@
-// 百度统计代码
-// 请将下面的XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX替换为您的百度统计站点ID
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?2ca52ad4e9043e4464f03cf44176c764";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
